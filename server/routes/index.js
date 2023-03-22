16e37bedcd82fc0dd4f9b855c3272eb62dfd0b7b
이성현 <marvel97@naver.com>
2023-03-22 17:35:49 +0900

    mpc shamir bls update

--- server/routes/index.js
+++ server/routes/index.js
@@ -216,11 +216,12 @@ router.post("/shamir", async (req, res) => {
     const { randomBytes } = require("crypto");
     const bip39 = require("bip39");
 
-    const PARTS = 10; // 5등분 했을 때
+    const PARTS = 5; // 5등분 했을 때
     const QUORUM = 3; // 3개 이상의 값이 있어야 복원 가능
 
     const mnemonic = bip39.generateMnemonic();
     const seed = await bip39.mnemonicToSeed(mnemonic);
+    console.log("seed: ", seed);
     const root = hdkey.fromMasterSeed(seed);
     const wallet = root.derivePath("m/44'/60'/0'/0/0").getWallet();
 
@@ -231,23 +232,16 @@ router.post("/shamir", async (req, res) => {
     // you can use any polyfill to covert string to Uint8Array
     const utf8Encoder = new TextEncoder();
     const utf8Decoder = new TextDecoder();
-    const secretBytes = utf8Encoder.encode(privateKey);
+    // const secretBytes = utf8Encoder.encode(privateKey);
+    const secretBytes = utf8Encoder.encode(mnemonic);
 
     // parts is a object whos keys are the part number and values are an Uint8Array
     const parts = split(randomBytes, PARTS, QUORUM, secretBytes);
     console.log("parts: ", parts);
 
-    function toHexString(byteArray) {
-      return Array.prototype.map
-        .call(byteArray, function (byte) {
-          return ("0" + (byte & 0xff).toString(16)).slice(-2);
-        })
-        .join("");
-    }
-
-    const result = [];
+    const privateKeys = [];
     for (const key in parts) {
-      result.push(toHexString(parts[key]));
+      privateKeys.push(`${key}${toHexString(parts[key])}`);
     }
 
     // we only need QUORUM of the parts to recover the secret
@@ -260,7 +254,7 @@ router.post("/shamir", async (req, res) => {
     // prints 'hello there'
     console.log("origin: ", utf8Decoder.decode(recoverd));
 
-    res.send({ result, privateKey, origin: utf8Decoder.decode(recoverd) });
+    res.send({ privateKeys, privateKey, origin: utf8Decoder.decode(recoverd) });
   } catch (error) {
     console.error(error.message);
     res.status(404).send({ message: error.message });
@@ -314,17 +308,11 @@ router.post("/mpc", async (req, res) => {
 
     const utf8Decoder = new TextDecoder();
 
-    function toByteArray(hexString) {
-      var result = [];
-      for (var i = 0; i < hexString.length; i += 2) {
-        result.push(parseInt(hexString.substr(i, 2), 16));
-      }
-      return result;
-    }
-
     const parse = {};
     for (let i = 0; i < string.length; i++) {
-      parse[i + 1] = new Uint8Array(toByteArray(string[i]));
+      parse[string[i].slice(0, 1)] = new Uint8Array(
+        toByteArray(string[i].slice(1, 200))
+      );
     }
     console.log("parse: ", parse);
 
@@ -340,5 +328,159 @@ router.post("/mpc", async (req, res) => {
     res.status(404).send({ message: error.message });
   }
 });
+
+router.post("/bls", async (req, res) => {
+  try {
+    const loadBLS = require("bls-signatures");
+    const BLS = await loadBLS();
+
+    // Creating keys and signatures
+    const seed = Uint8Array.from([
+      0, 50, 6, 244, 24, 199, 1, 25, 52, 88, 192, 19, 18, 12, 89, 6, 220, 18,
+      102, 58, 209, 82, 12, 62, 89, 110, 182, 9, 44, 20, 254, 22,
+    ]);
+
+    const sk = BLS.AugSchemeMPL.key_gen(seed);
+    const pk = sk.get_g1();
+
+    const message = Uint8Array.from([1, 2, 3, 4, 5]);
+    const signature = BLS.AugSchemeMPL.sign(sk, message);
+
+    let ok = BLS.AugSchemeMPL.verify(pk, message, signature);
+    console.log("ok: ", ok);
+
+    // Serializing keys and signatures to bytes
+    const skBytes = sk.serialize();
+    const pkBytes = pk.serialize();
+    const signatureBytes = signature.serialize();
+
+    console.log("skBytes: ", BLS.Util.hex_str(skBytes));
+    console.log("pkBytes: ", BLS.Util.hex_str(pkBytes));
+    console.log("signatureBytes: ", BLS.Util.hex_str(signatureBytes));
+
+    // Loading keys and signatures from bytes
+    const skc = BLS.PrivateKey.from_bytes(skBytes, false);
+    const pkc = BLS.G1Element.from_bytes(pkBytes);
+    const signaturec = BLS.G2Element.from_bytes(signatureBytes);
+
+    // Create aggregate signatures
+    // 1. Generate some more private keys
+    seed[0] = 1;
+    const sk1 = BLS.AugSchemeMPL.key_gen(seed);
+    seed[0] = 2;
+    const sk2 = BLS.AugSchemeMPL.key_gen(seed);
+    const message2 = Uint8Array.from([1, 2, 3, 4, 5, 6, 7]);
+
+    // 2. Generate first sig
+    const pk1 = sk1.get_g1();
+    const sig1 = BLS.AugSchemeMPL.sign(sk1, message);
+
+    // 3. Generate second sig
+    const pk2 = sk2.get_g1();
+    const sig2 = BLS.AugSchemeMPL.sign(sk2, message2);
+
+    // 4. Signatures can be non-interactively combined by anyone
+    const aggSig = BLS.AugSchemeMPL.aggregate([sig1, sig2]);
+
+    let ok2 = BLS.AugSchemeMPL.aggregate_verify(
+      [pk1, pk2],
+      [message, message2],
+      aggSig
+    );
+    console.log("ok2: ", ok2);
+
+    // Arbitrary trees of aggregates
+    seed[0] = 3;
+    const sk3 = BLS.AugSchemeMPL.key_gen(seed);
+    const pk3 = sk3.get_g1();
+    const message3 = Uint8Array.from([100, 2, 254, 88, 90, 45, 23]);
+    const sig3 = BLS.AugSchemeMPL.sign(sk3, message3);
+
+    const aggSigFinal = BLS.AugSchemeMPL.aggregate([aggSig, sig3]);
+    let ok3 = BLS.AugSchemeMPL.aggregate_verify(
+      [pk1, pk2, pk3],
+      [message, message2, message3],
+      aggSigFinal
+    );
+    console.log("ok3: ", ok3);
+
+    // Very fast verification with Proof of Possession cheme
+    // 1. If the same message is signed, you can use Proof of Posession (PopScheme) for efficiency
+    // 2. A proof of possession MUST be passed around with the PK to ensure security.
+    const popSig1 = BLS.PopSchemeMPL.sign(sk1, message);
+    const popSig2 = BLS.PopSchemeMPL.sign(sk2, message);
+    const popSig3 = BLS.PopSchemeMPL.sign(sk3, message);
+    const pop1 = BLS.PopSchemeMPL.pop_prove(sk1);
+    const pop2 = BLS.PopSchemeMPL.pop_prove(sk2);
+    const pop3 = BLS.PopSchemeMPL.pop_prove(sk3);
+
+    ok = BLS.PopSchemeMPL.pop_verify(pk1, pop1);
+    console.log("ok: ", ok); // true
+    ok = BLS.PopSchemeMPL.pop_verify(pk2, pop2);
+    console.log("ok: ", ok); // true
+    ok = BLS.PopSchemeMPL.pop_verify(pk3, pop3);
+    console.log("ok: ", ok); // true
+
+    const popSigAgg = BLS.PopSchemeMPL.aggregate([popSig1, popSig2, popSig3]);
+    ok = BLS.PopSchemeMPL.fast_aggregate_verify(
+      [pk1, pk2, pk3],
+      message,
+      popSigAgg
+    );
+    console.log("ok: ", ok); // true
+
+    // 3. Aggregate public key, indistinguishable from a single public key
+    const popAggPk = pk1.add(pk2).add(pk3);
+    ok = BLS.PopSchemeMPL.verify(popAggPk, message, popSigAgg);
+    console.log("ok: ", ok); // true
+
+    // 4. Aggregate private keys
+    const aggSk = BLS.PrivateKey.aggregate([sk1, sk2, sk3]);
+    ok = BLS.PopSchemeMPL.sign(aggSk, message).equal_to(popSigAgg);
+    console.log("ok: ", ok); // true
+
+    // HD keys using EIP-2333
+    // 1. You can derive 'child' keys from any key, to create arbitrary trees. 4 byte indeces are used.
+    // 2. Hardened (more secure, but no parent pk -> child pk)
+    const masterSk = BLS.AugSchemeMPL.key_gen(seed);
+    const child = BLS.AugSchemeMPL.derive_child_sk(masterSk, 152);
+    const grandChild = BLS.AugSchemeMPL.derive_child_sk(child, 952);
+
+    // Unhardened (less secure, but can go from parent pk -> child pk), BIP32 style
+    const masterPk = masterSk.get_g1();
+    const childU = BLS.AugSchemeMPL.derive_child_sk_unhardened(masterSk, 22);
+    const grandchildU = BLS.AugSchemeMPL.derive_child_sk_unhardened(childU, 0);
+
+    const childUPk = BLS.AugSchemeMPL.derive_child_pk_unhardened(masterPk, 22);
+    const grandchildUPk = BLS.AugSchemeMPL.derive_child_pk_unhardened(
+      childUPk,
+      0
+    );
+
+    ok = grandchildUPk.equal_to(grandchildU.get_g1());
+    console.log("ok: ", ok); // true
+
+    res.send({ message: true });
+  } catch (error) {
+    console.error(error.message);
+    res.status(404).send({ message: error.message });
+  }
+});
+
+function toHexString(byteArray) {
+  return Array.prototype.map
+    .call(byteArray, function (byte) {
+      return ("0" + (byte & 0xff).toString(16)).slice(-2);
+    })
+    .join("");
+}
+
+function toByteArray(hexString) {
+  var result = [];
+  for (var i = 0; i < hexString.length; i += 2) {
+    result.push(parseInt(hexString.substr(i, 2), 16));
+  }
+  return result;
+}
 
 module.exports = router;
