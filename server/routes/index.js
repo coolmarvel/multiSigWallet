933cbdeb1c2e8490e850430ba03d68ec6a2592e2
이성현 <marvel97@naver.com>
2023-03-22 11:31:27 +0900

    shamir's secret sharing update

--- server/routes/index.js
+++ server/routes/index.js
@@ -209,11 +209,132 @@ router.post("/transfer/execute", async (req, res) => {
   }
 });
 
+router.post("/shamir", async (req, res) => {
+  try {
+    const { hdkey } = require("ethereumjs-wallet");
+    const { split, join } = require("shamir");
+    const { randomBytes } = require("crypto");
+    const bip39 = require("bip39");
+
+    const PARTS = 10; // 5등분 했을 때
+    const QUORUM = 3; // 3개 이상의 값이 있어야 복원 가능
+
+    const mnemonic = bip39.generateMnemonic();
+    const seed = await bip39.mnemonicToSeed(mnemonic);
+    const root = hdkey.fromMasterSeed(seed);
+    const wallet = root.derivePath("m/44'/60'/0'/0/0").getWallet();
+
+    const privateKey = wallet.getPrivateKeyString();
+    console.log("mnemonic: ", mnemonic);
+    console.log("privateKey: ", privateKey);
+
+    // you can use any polyfill to covert string to Uint8Array
+    const utf8Encoder = new TextEncoder();
+    const utf8Decoder = new TextDecoder();
+    const secretBytes = utf8Encoder.encode(privateKey);
+
+    // parts is a object whos keys are the part number and values are an Uint8Array
+    const parts = split(randomBytes, PARTS, QUORUM, secretBytes);
+    console.log("parts: ", parts);
+
+    function toHexString(byteArray) {
+      return Array.prototype.map
+        .call(byteArray, function (byte) {
+          return ("0" + (byte & 0xff).toString(16)).slice(-2);
+        })
+        .join("");
+    }
+
+    const result = [];
+    for (const key in parts) {
+      result.push(toHexString(parts[key]));
+    }
+
+    // we only need QUORUM of the parts to recover the secret
+    delete parts[1];
+    delete parts[5];
+
+    // recoverd is an Uint8Array
+    const recoverd = join(parts);
+
+    // prints 'hello there'
+    console.log("origin: ", utf8Decoder.decode(recoverd));
+
+    res.send({ result, privateKey, origin: utf8Decoder.decode(recoverd) });
+  } catch (error) {
+    console.error(error.message);
+    res.status(404).send({ message: error.message });
+  }
+});
+
+router.post("/sss", async (req, res) => {
+  try {
+    const { hdkey } = require("ethereumjs-wallet");
+    const sss = require("shamirs-secret-sharing");
+    const bip39 = require("bip39");
+
+    const mnemonic = bip39.generateMnemonic();
+    const seed = await bip39.mnemonicToSeed(mnemonic);
+    const root = hdkey.fromMasterSeed(seed);
+    const wallet = root.derivePath("m/44'/60'/0'/0/0").getWallet();
+
+    const privateKey = wallet.getPrivateKeyString();
+    console.log("mnemonic: ", mnemonic);
+    console.log("privateKey: ", privateKey);
+
+    const secret = Buffer.from(privateKey);
+    console.log("secret: ", secret);
+
+    const shares = sss.split(privateKey, { shares: 5, threshold: 3 });
+    console.log("shares: ", shares);
+
+    const test = [];
+    test.push(shares[0]);
+    test.push(shares[1]);
+    test.push(shares[2]);
+
+    const recoverd = sss.combine(test);
+    console.log("recoverd: ", recoverd);
+
+    const origin = recoverd.toString();
+    console.log("origin: ", origin);
+
+    res.send({ message: true });
+  } catch (error) {
+    console.error(error.message);
+    res.status(404).send({ message: error.message });
+  }
+});
+
 router.post("/mpc", async (req, res) => {
   try {
-    const wallets = await web3.eth.accounts.wallet.create(2);
-    console.log(wallets);
-    res.send({ messate: true });
+    const { string } = req.body;
+
+    const { join } = require("shamir");
+
+    const utf8Decoder = new TextDecoder();
+
+    function toByteArray(hexString) {
+      var result = [];
+      for (var i = 0; i < hexString.length; i += 2) {
+        result.push(parseInt(hexString.substr(i, 2), 16));
+      }
+      return result;
+    }
+
+    const parse = {};
+    for (let i = 0; i < string.length; i++) {
+      parse[i + 1] = new Uint8Array(toByteArray(string[i]));
+    }
+    console.log("parse: ", parse);
+
+    // recoverd is an Uint8Array
+    const recoverd = join(parse);
+
+    // prints 'hello there'
+    console.log("origin: ", utf8Decoder.decode(recoverd));
+
+    res.send({ origin: utf8Decoder.decode(recoverd) });
   } catch (error) {
     console.error(error.message);
     res.status(404).send({ message: error.message });
